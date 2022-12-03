e6942bfe2c1dd2ef0f04fa050f90bace2fb41638
faris <farisariep@gmail.com>
2022-12-03 21:48:40 +0700

    Initial Commit

---
+++ Meat Solution/Meat_SolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meat_SolutionApp.swift
+//  Meat Solution
+//
+//  Created by yoga arie on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Meat_SolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
