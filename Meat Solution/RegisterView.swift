a0351694f2e35d5cb58ee1b95f8ef928fc3eb484
Caroline Chan <carolinechan304@gmail.com>
2022-12-05 12:03:26 +0700

    Create register view

---
+++ Meat Solution/RegisterView.swift
@@ -0,0 +1,80 @@
+//
+//  RegisterView.swift
+//  Meat Solution
+//
+//  Created by Caroline Chan on 05/12/22.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var fullname: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Register")
+                    .font(.title.weight(.bold))
+                    .foregroundColor(.accentColor)
+                    .padding(.vertical)
+                
+                Text("FULL NAME")
+                    .font(.caption.weight(.semibold))
+                    .foregroundColor(.accentColor)
+                TextField("", text: $fullname)
+                    .frame(maxWidth: .infinity, maxHeight: 51)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("AccentColor"), lineWidth: 1)
+                    )
+                    .padding(.bottom)
+                
+                Text("E-MAIL")
+                    .font(.caption.weight(.semibold))
+                    .foregroundColor(.accentColor)
+                TextField("", text: $email)
+                    .frame(maxWidth: .infinity, maxHeight: 51)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("AccentColor"), lineWidth: 1)
+                    )
+                    .padding(.bottom)
+                
+                Text("PASSWORD")
+                    .font(.caption.weight(.semibold))
+                    .foregroundColor(.accentColor)
+                SecureField("", text: $password)
+                    .frame(maxWidth: .infinity, maxHeight: 51)
+                    .cornerRadius(6)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("AccentColor"), lineWidth: 1)
+                    )
+                    .padding(.bottom)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Sign Up")
+                        .font(.title2.weight(.bold))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: 54)
+                        .background(Color("AccentColor"))
+                        .cornerRadius(16)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
