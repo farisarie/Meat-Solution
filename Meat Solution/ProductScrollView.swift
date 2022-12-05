a3f2fa442a9e9d566ca0e3161f1f04b93b487d11
Caroline Chan <carolinechan304@gmail.com>
2022-12-05 14:17:30 +0700

    Create product scroll view

---
+++ Meat Solution/ProductScrollView.swift
@@ -0,0 +1,50 @@
+//
+//  ProductScrollView.swift
+//  Meat Solution
+//
+//  Created by Caroline Chan on 05/12/22.
+//
+
+import SwiftUI
+
+struct ProductScrollView: View {
+    var body: some View {
+        ZStack {
+            Color.gray
+            
+            ScrollView {
+                VStack {
+                    ForEach(0..<10) { _ in
+                        ZStack {
+                            Color.white
+                            
+                            HStack {
+                                Image(systemName: "person")
+                                    .resizable()
+                                    .frame(width: 95, height: 95)
+                                
+                                Spacer()
+                                
+                                Text("Item")
+                                    .font(.title)
+                            }
+                            .padding()
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: 101)
+                        .background(.white)
+                        .cornerRadius(10)
+                    }
+                }
+                .padding()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 350)
+        .cornerRadius(20)
+    }
+}
+
+struct ProductScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductScrollView()
+    }
+}
